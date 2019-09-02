871ff181be7a3a979f9e483afae9c4aee900b068
Ari Putra <ariteknologi@gmail.com>
2019-09-02 22:12:57 +0700

    add user skill model n migration

---
+++ app/Models/UserSkill.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserSkill extends Model
+{
+    protected $table = 'user_skills';
+
+    protected $fillable = [
+        'name','user_id'
+    ];
+}
---
+++ database/migrations/2019_09_02_151101_create_user_skills_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('user_skills')) {
+            Schema::create('user_skills', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('name');
+                $table->unsignedBigInteger('user_id');
+                $table->timestamps();
+
+                if (Schema::hasTable('users')) {
+                    $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+                }
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_skills');
+    }
+}
