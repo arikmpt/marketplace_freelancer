26ef3bf671051843df0471525fe1848b4b459370
Ari Putra <ariteknologi@gmail.com>
2019-09-11 17:38:33 +0700

    redesign homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\User;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('pages.homepage.index');
+        return view('pages.homepage.index')
+            ->with([
+                'projects' => Project::where('is_approve', 1)->count(),
+                'users' => User::count()
+            ]);
     }
 }
